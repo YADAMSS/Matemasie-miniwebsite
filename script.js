5c551d4b9623184fb561fae3745f7841907dfc0c
ADAM YEO <47873757+YADAMSS@users.noreply.github.com>
2024-04-06 19:06:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// JavaScript for Matemasie Font Showcase
+
+$(document).ready(function(){
+  // Change font size based on selection
+  $('#font-size').change(function(){
+    var fontSize = $(this).val();
+    $('.font-demo').css('font-size', fontSize);
+  });
+
+  // Change font color based on selection
+  $('#font-color').change(function(){
+    var fontColor = $(this).val();
+    $('.font-demo').css('color', fontColor);
+  });
+});
 
